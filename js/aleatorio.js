f43e7250a20ec77a251895c4696c69b545af11f5
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-09-10 07:33:30 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,8 @@
 const nomes = ["Juca", "Emerson", "Osvaldo", "Pipokinha", "Deolane", "Virginia"];
 
-function aleatorio(lista){
+export function aleatorio(lista){
   const posicao = Math.floor(Math.random()* lista.length);
   return lista[posicao];
 }
 
-const nome = aleatorio(nomes) 
+export const nome = aleatorio(nomes)
