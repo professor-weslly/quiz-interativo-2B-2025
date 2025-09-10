1b643e280c9fb224ffe7dd2b463419c5f437e8ed
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-09-10 07:29:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
@@ -16,6 +18,7 @@ botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo() {
   atual = 0;
+  historiaFinal = "";
   telaInicial.style.display = 'none';
   caixaPerguntas.classList.remove(".mostrar");
   caixaAlternativas.classList.remove(".mostrar");
@@ -34,7 +37,16 @@ function mostraPergunta() {
   mostraAlternativas();
 }
 
-function mostraAlternativas() {}
+function mostraAlternativas() {
+  for(const alternativa of perguntaAtual.alternativas){
+      const botaoAlternativas = document.creatElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
+      caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
+function respostaSelecionada(opcaoSelecionada) {}
 
 function mostraResultado() {
   caixaPerguntas.textContent = `Após tudo isso, ${nome} descobriu que`;
