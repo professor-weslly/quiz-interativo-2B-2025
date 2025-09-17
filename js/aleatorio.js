3ddcb5d10cd75e68b552789837690a0b64b6b69d
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-09-17 07:42:14 -0300

    Delete js/aleatorio.js

--- js/aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Juca", "Emerson", "Osvaldo", "Pipokinha", "Deolane", "Virginia"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
