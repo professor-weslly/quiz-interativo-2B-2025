7eae9eff81e708027102a923512c883c8f672099
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-09-17 07:40:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,121 @@
-import {aleatorio, nome} from './aleatorio.js';
-import {perguntas} from './perguntas.js';
+//perguntas
+const perguntas = [
+    {
+        enunciado: "Você prefere nike ou adidas?",  //a pergunta
+        alternativas: [
+            {
+                texto: "nike", //primeira alternativa
+                afirmacao: [
+                    "você tem bom gosto", //comentário para a resposta
+                    "afirmação 2"        //segundo comentário
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "adidas",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 1,
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            {
+                texto: "nike",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "adidas",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 4",
+        alternativas: [
+            {
+                texto: "nike",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "adidas",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "Você prefere nike ou adidas?",
+        alternativas: [
+            {
+                texto: "nike",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "adidas",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 5",
+        alternativas: [
+            {
+                texto: "nike",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "adidas",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+            }
+        ]
+    }
+]
+
+//aleatorio
+const nomes = ["Juca", "Emerson", "Osvaldo", "Pipokinha", "Deolane", "Virginia"];
+
+function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+const nome = aleatorio(nomes)
+
+//script principal
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
